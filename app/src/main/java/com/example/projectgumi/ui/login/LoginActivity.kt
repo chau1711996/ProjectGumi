5ea4f3c3994544a4ce8ba78746e7038b1ed3cd73
chau1711996 <chau1711996@gmail.com>
2021-05-12 15:18:27 +0700

    login-facebook

--- app/src/main/java/com/example/projectgumi/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/projectgumi/ui/login/LoginActivity.kt
@@ -6,15 +6,23 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import coil.load
 import com.example.projectgumi.R
 import com.example.projectgumi.databinding.ActivityLoginBinding
 import com.example.projectgumi.utils.Utils.RC_SIGN_IN
 import com.example.projectgumi.utils.Utils.TAG
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -25,6 +33,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var callbackManager: CallbackManager
 
     override fun onStart() {
         super.onStart()
@@ -35,10 +44,14 @@ class LoginActivity : AppCompatActivity() {
 
     private fun updateUI(user: FirebaseUser?) {
         binding.layoutLogin.visibility = View.GONE
+        binding.buttonLoginFb.visibility = View.VISIBLE
+        binding.buttonLoginGg.visibility = View.VISIBLE
         user?.let {
             binding.layoutLogin.visibility = View.VISIBLE
             binding.imageUser.load(it.photoUrl)
             binding.textEmail.text = it.displayName
+            binding.buttonLoginFb.visibility = View.GONE
+            binding.buttonLoginGg.visibility = View.GONE
         }
     }
 
@@ -61,15 +74,41 @@ class LoginActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        binding.buttonLogin.setOnClickListener {
-            signIn()
+        callbackManager = CallbackManager.Factory.create()
+
+        LoginManager.getInstance().registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        })
+
+        binding.buttonLoginGg.setOnClickListener {
+            signInGg()
+        }
+        binding.buttonLoginFb.setOnClickListener {
+            signInFb()
         }
         binding.buttonLogout.setOnClickListener {
             signOut()
         }
+
     }
 
-    private fun signIn() {
+    private fun signInFb() {
+        LoginManager.getInstance().logInWithReadPermissions(this, arrayListOf("public_profile", "user_friends"))
+    }
+
+    private fun signInGg() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
@@ -82,6 +121,9 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
@@ -109,6 +151,29 @@ class LoginActivity : AppCompatActivity() {
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d("TAG", "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     updateUI(null)
                 }
             }
