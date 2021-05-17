9aba6629652247da82e641bc7098df9b60f6ea47
chau1711996 <chau1711996@gmail.com>
2021-05-17 16:24:37 +0700

    create-ui

---
+++ app/src/main/java/com/example/projectgumi/ui/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.projectgumi.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectgumi.R
+import com.example.projectgumi.ui.login.LoginActivity
+import kotlinx.coroutines.*
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(2000)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
